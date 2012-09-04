4c6afd8c22697e308ee2e00dfd82d34091d0007f
Brian McClain <brianmmcclain@gmail.com>
2012-09-04 17:58:28 -0400

    Fix VIM connection argument syntax to be a hash not a non-existant array type, as well as make new connection each request while I figure out if we have to deal with dropped connections. Slower but more reliable in the meantime

--- lib/cfcm/iaas/vsphere.rb
+++ lib/cfcm/iaas/vsphere.rb
@@ -5,11 +5,14 @@ module CFCM
     class vSphere
         
       def initalize(host, user, password)
-        @vim = VIM.connect [:host => host, :user => user, :password => password]
+        @host = host
+        @user = user
+        @password = password
       end
       
       def power_off_vm(datacenter, vm)
-        dc = @vim.serviceInstance.content.rootFolder.traverse(datacenter, VIM::Datacenter) or abort "datacenter not found"
+        vim = VIM.connect {:host=>@host, :user=>@user, :password=>@password, :insecure=>true}
+        dc = vim.serviceInstance.content.rootFolder.traverse(datacenter, VIM::Datacenter) or abort "datacenter not found"
         vm = dc.vmFolder.traverse(vm, VIM::VirtualMachine) or abort "VM not found"
         vm.PowerOffVM_Task.wait_for_completion
       end
