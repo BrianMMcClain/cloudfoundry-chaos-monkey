be19c1b1bdd67c5307540ec147487b290a14ae00
Brian McClain <brianmmcclain@gmail.com>
2012-09-04 00:10:21 -0400

    vSphere IaaS code to poweroff VM based of rbvmomi example

---
+++ lib/cfcm/iaas/vsphere.rb
@@ -0,0 +1,19 @@
+require 'rbvmomi'
+
+module CFCM
+  module IAAS
+    class vSphere
+        
+      def initalize(host, user, password)
+        @vim = VIM.connect [:host => host, :user => user, :password => password]
+      end
+      
+      def power_off_vm(datacenter, vm)
+        dc = @vim.serviceInstance.content.rootFolder.traverse(datacenter, VIM::Datacenter) or abort "datacenter not found"
+        vm = dc.vmFolder.traverse(vm, VIM::VirtualMachine) or abort "VM not found"
+        vm.PowerOffVM_Task.wait_for_completion
+      end
+        
+    end
+  end
+end
