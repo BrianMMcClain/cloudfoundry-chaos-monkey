cd946ad5f6eec21c403bfe4a933123d735556115
Brian McClain <brianmmcclain@gmail.com>
2012-12-09 17:53:18 -0500

    Changed tests to use vcr instead of hand-made fixtures

--- spec/session_spec.rb
+++ spec/session_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe CFCM::CF::Session do
 	it "should request a Cloud Foundry token" do
-		stub_request(:get, "http://api.vcap.local/info").
-        	with(:headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip, deflate', 'Content-Length'=>'0', 'User-Agent'=>'Ruby'}).
-         	to_return(:status => 200, :body => "", :headers => {})
-
-		session = CFCM::CF::Session.new("http://api.vcap.local", "someuser", "somepassword")
-
+		VCR.use_cassette('cf_login') do
+			session = CFCM::CF::Session.new("http://api.cloudfoundry.com", "someuser", "somepassword")
+			session.is_logged_in?.should be_true
+			session.client.token.should == "fake f@k3t0k3n"
+			session.client.current_user.email.should == "someuser"
+		end
 	end	
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,16 @@
 require_relative '../lib/cfcm'
 require 'webmock/rspec'
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  c.hook_into :webmock
+end
+
+RSpec.configure do |config|
+	config.before(:all) do
+		VCR.use_cassette('cf_login') do
+			@client = CFCM::CF::Session.new("http://api.cloudfoundry.com", "someuser", "somepassword")
+		end
+	end
+end
